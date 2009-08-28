1e4461f87750c60fc5ed67734ba9273553302228
John D. Rowell <me@jdrowell.com>
2009-08-28 10:26:40 -0300

    Enhanced "best commit" code.

--- crowd.rb
+++ crowd.rb
@@ -22,7 +22,7 @@ class Crowdsource
     @leaders = []
     @doc.css('.leaderboard tr')[1..-1].each do |tr|
       tr = tr.css('td')
-      entry = tr[0].css('a').inner_html
+      entry = tr[0].css('a').attr('href') #inner_html
       correct = tr[1].inner_html
       percent = tr[2].inner_html
       link = tr[3].css('a').attr('href')
@@ -44,16 +44,35 @@ class Crowdsource
   end
 
   def fetch_results
-    @leaders[2..-1].each do |leader|
+    @leaders[0..40].each do |leader|
       print "Leader: #{leader[0]}\n"
-      print "  determining last commit\n"
+      print "  determining best commit\n"
       #@doc = Nokogiri::HTML(open("#{leader[3]}/commits/master/results.txt"))
-      @doc = Nokogiri::HTML(open("#{leader[3]}/tree/master"))
-      a = @doc.css('#commit .group .machine a').first
-      commit = a.attribute('href').to_s.split('/')[-1]
+      #@doc = Nokogiri::HTML(open("#{leader[3]}/tree/master"))
+      @doc = Nokogiri::HTML(open("http://contest.github.com#{leader[0]}"))
+      #a = @doc.css('#commit .group .machine a').first
+      #commit = a.attribute('href').to_s.split('/')[-1]
+      trs = @doc.css('table')[1].css('tr')
+      #debugger
+      max = 0
+      commit = ''
+      trs.each do |tr|
+        score = tr.css('td strong').inner_html.to_i
+        #print "score: #{score}   "
+        if score > max
+          commit = tr.css('td')[4].css('a').attr('href').split('/')[-1]
+          max = score
+        end
+      end
+      raise "wtf" if commit.empty?
       url = "#{leader[3]}/raw/#{commit}/results.txt"
       print "  fetching #{url}\n"
-      results = open(url).read
+      begin
+        results = open(url).read
+      rescue
+        print "Whatever HTTP error\n"
+        results = ''
+      end
       File.open(filename(leader[0]), 'w+').write(results)
     end
   end
@@ -81,12 +100,13 @@ class Crowdsource
         @user_repos[user][:weight] = 0 unless @user_repos[user].has_key?(:weight)
         @user_repos[user][:weight] += weight
         repos.each do |repo|
+          raise "can't be zero" if repo == 0
           @user_repos[user][repo] = 0 unless @user_repos[user].has_key?(repo)
           @user_repos[user][repo] += weight
         end
       end
     end
-    @user_repos.delete(0)
+    #@user_repos.delete(0)
     f = File.open('results.txt', 'w+')
     @user_repos.sort_by { |x| -x[0][:weight] }.each do |user, repo_hash|
       repo_hash.delete(:weight)
